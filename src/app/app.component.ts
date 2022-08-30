c5af8cc1f0330e73d0807b2f4c472043e0e06212
Kamyar <kamyarkarimi98@gmail.com>
2022-08-30 16:17:23 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'testproj';
+  public newMemberName: string = '';
+  public members: string[] = [];
+  public errorMessage: string = '';
+
+  public addMemberToArray(newMember: string): void {
+    if (newMember == '') {
+      this.errorMessage = 'Name input cannot be empty';
+    } else {
+      this.members.push(newMember);
+      this.errorMessage = '';
+    }
+    console.log(this.members);
+  }
+
+  public generateTeams(numberOfTeams: string): void {
+    if (Number(numberOfTeams) <= 0) {
+      this.errorMessage = 'Number has to be more than 0';
+    } else {
+      this.errorMessage = '';
+    }
+  }
+
+  public clearInput(): string {
+    return ' ';
+  }
 }
